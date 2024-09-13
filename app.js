55758f160338e3f3df5d892be54128904856f660
iamarale <ismailarales@gmail.com>
2024-09-12 19:25:34 -0500

    favorite exercise: added fav button and stars when toggled

--- app.js
+++ app.js
@@ -16,3 +16,41 @@ function removeModuleChild() {
 }
 
 document.addEventListener("click", removeModuleChild);
+
+// add favorites exercise
+const nanoCardTitle = document.getElementsByClassName(
+  "card--nanodegree__title"
+);
+
+// adds button under title for each card
+for (const card of nanoCardTitle) {
+  // creates btn for each card
+  const button = document.createElement("button");
+  button.textContent = "Add to Favorites";
+  button.classList.add("button");
+  button.classList.add("button--primary");
+  card.appendChild(button);
+
+  // click event
+  button.addEventListener("click", function (e) {
+    // defaults
+    e.preventDefault();
+    e.target.classList.toggle("favorite");
+    console.log(e.target.classList.contains("favorite"));
+
+    // star element
+    const stars = document.createElement("p");
+    stars.textContent = "🌟🌟🌟";
+
+    // toggle logic
+    if (e.target.classList.contains("favorite")) {
+      e.target.textContent = "Remove from favorite";
+      e.target.appendChild(stars);
+    } else {
+      e.target.textContent = "Add to Favorites";
+      stars.remove();
+    }
+
+    //
+  });
+}
